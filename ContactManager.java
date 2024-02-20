2fafcb2949007a779272aa6ee741a580b48d6ccf
Your Name <you@example.com>
2024-02-19 21:53:17 -0500

    Commented Out getName() Method

--- ContactManager.java
+++ ContactManager.java
@@ -14,18 +14,28 @@ class ContactManager {
 
     // Method to add a new Contact
     public void addContact(Contact contact) {
-        // Your code here
+        contacts.add(contact);
     }
 
     // Method to remove a Contact by name
     public void removeContact(String name) {
-        // Your code here
+        Iterator<Contact> iterator = contacts.iterator();
+        while (iterator.hasNext()) {
+            Contact contact = iterator.next();
+            if (contact.getName().equals(name)) {
+                iterator.remove();
+                System.out.println("Contact '" + name + "' removed successfully.");
+                return; // Stop searching after finding and removing the contact
+            }
+        }
+        System.out.println("Contact '" + name + "' not found.");
     }
 
     // Method to list all Contacts
     public void listContacts() {
-        // Your code here
+        System.out.println("List of Contacts:");
+        for (Contact contact : contacts) {
+            System.out.println(contact);
+        }
     }
-
-
 }
